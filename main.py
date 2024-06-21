8ca2ec9460c7afa52d0bec39798a38348a9eb270
andygutterman <andygutterman@outlook.com>
2024-06-21 12:22:17 -0400

    minor changes, increased compatibility for intersecting words

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 import random
 
-def fill_grid(wordsearch):  # Fill spaces with value '0' with random characters
-    for row in wordsearch:
-        for i in range(len(row)):
-            if row[i] == 0:
-                randchar = chr(random.randint(65, 90))
-                row[i] = randchar
 
-def grid_print(wordsearch):
-    print("\n\nWord Search:")
-    for row in wordsearch:
-        elements_to_print = []
-        for element in row:
-            if element == "1":
-                elements_to_print.append("\033[1;32m" + element + "\033[0m")
-            else:
-                elements_to_print.append(element)
-        row_string = " ".join(str(element) for element in elements_to_print)
-        print(row_string)
+def takeWords():
+    words = []
+    spacesRemaining = size * size
+    while spacesRemaining > 0:  # Take word from user and add to list, stopping at done
+        word = input("Enter a word, 'done' when finished:")
+        word = word.upper()  # Convert word to uppercase
+        if word == 'DONE' or spacesRemaining < 0:  # Break loop if user is done
+            break
+        if len(word) <= size & len(word) <= spacesRemaining:  # Ensures word is small enough to fit
+            words.append(word)
+            spacesRemaining = spacesRemaining - len(word)
+            print("You have " + str(spacesRemaining) + "characters left")
+        else:
+            print("The word you've typed is too large, please choose another word")
+    return words
+
 
 def place_Diagonal(letters, size, wordsearch, max_attempts=1000):
     attempts = 0
@@ -27,7 +26,8 @@ def place_Diagonal(letters, size, wordsearch, max_attempts=1000):
         column_num = random.randint(0, size - len(letters))
 
         for i in range(len(letters)):
-            if wordsearch[row_num + i][column_num + i] not in (0, letters[i]):
+            current_char = wordsearch[row_num + i][column_num + i]
+            if current_char != 0 and current_char != letters[i]:
                 can_place_word = False
                 break
 
@@ -38,15 +38,18 @@ def place_Diagonal(letters, size, wordsearch, max_attempts=1000):
         attempts += 1
     return False
 
+
 def place_horizontal(letters, size, wordsearch, max_attempts=1000):
     attempts = 0
     while attempts < max_attempts:
         can_place_word = True
         row_num = random.randint(0, size - 1)
         column_num = random.randint(0, size - len(letters))
+        intersections = 0
 
         for i in range(len(letters)):
-            if wordsearch[row_num][column_num + i] not in (0, letters[i]):
+            current_char = wordsearch[row_num][column_num + i]
+            if current_char != 0 and current_char != letters[i]:
                 can_place_word = False
                 break
 
@@ -57,6 +60,7 @@ def place_horizontal(letters, size, wordsearch, max_attempts=1000):
         attempts += 1
     return False
 
+
 def place_vertical(letters, size, wordsearch, max_attempts=1000):
     attempts = 0
     while attempts < max_attempts:
@@ -65,7 +69,8 @@ def place_vertical(letters, size, wordsearch, max_attempts=1000):
         column_num = random.randint(0, size - 1)
 
         for i in range(len(letters)):
-            if wordsearch[row_num + i][column_num] not in (0, letters[i]):
+            current_char = wordsearch[row_num + i][column_num]
+            if current_char != 0 and current_char != letters[i]:
                 can_place_word = False
                 break
 
@@ -76,54 +81,75 @@ def place_vertical(letters, size, wordsearch, max_attempts=1000):
         attempts += 1
     return False
 
-def placeWords(wordsearch, words):
-    size = len(wordsearch)
-    words.sort(key=len, reverse=True)  # Sort words by length in descending order
+def placeWords(wordSearch, fullWords):
+    size = len(wordSearch)
+    fullWords.sort(key=len, reverse=True)  # Sort words by length in descending order
 
     LimitWords = 0
-    for word in words:
+    for word in fullWords:
         if len(word) == size:
             LimitWords += 1
 
     if LimitWords > 1:
         print(f"There are {LimitWords} words with length equal to the size of the array.")
-        for word in words:
+        for word in fullWords:
             letters = list(word)
             placed = False
             while not placed:
                 r = random.randint(1, 2)
                 if r == 1:
-                    placed = place_vertical(letters, size, wordsearch)
+                    placed = place_vertical(letters, size, wordSearch)
                     if placed:
                         print(word + " placed vertically")
                 elif r == 2:
-                    placed = place_horizontal(letters, size, wordsearch)
+                    placed = place_horizontal(letters, size, wordSearch)
                     if placed:
                         print(word + " placed horizontally")
     else:
-        for word in words:
+        for word in fullWords:
             letters = list(word)
             placed = False
             while not placed:
                 r = random.randint(1, 3)
                 if r == 1:
-                    placed = place_vertical(letters, size, wordsearch)
+                    placed = place_vertical(letters, size, wordSearch)
                     if placed:
                         print(word + " placed vertically")
                 elif r == 2:
-                    placed = place_horizontal(letters, size, wordsearch)
+                    placed = place_horizontal(letters, size, wordSearch)
                     if placed:
                         print(word + " placed horizontally")
                 elif r == 3:
-                    placed = place_Diagonal(letters, size, wordsearch)
+                    placed = place_Diagonal(letters, size, wordSearch)
                     if placed:
                         print(word + " placed diagonally")
 
-if __name__ == '__main__':
-    size = 5
-    wordsearch = [[0] * size for i in range(size)]
-    words = ["TOOLS", "FOOLS", "COOL"]
 
-    placeWords(wordsearch, words)
-    fill_grid(wordsearch)
-    grid_print(wordsearch)
+def fill_grid(wordSearch):  # Fill spaces with value '0' with random characters
+    for row in wordSearch:
+        for i in range(len(row)):
+            if row[i] == 0:
+                randchar = chr(random.randint(65, 90))
+                row[i] = randchar
+
+
+def grid_print(wordSearch):
+    print("\n\nWord Search:")
+    for row in wordSearch:
+        elements_to_print = []
+        for element in row:
+            if element == "1":
+                elements_to_print.append("\033[1;32m" + element + "\033[0m")
+            else:
+                elements_to_print.append(element)
+        row_string = " ".join(str(element) for element in elements_to_print)
+        print(row_string)
+
+
+if __name__ == '__main__':
+    size = int(input("Enter a size for the wordSearch:\n>>> "))
+    wordSearch = [[0] * size for i in range(size)]
+    words = takeWords()
+    placeWords(wordSearch, words)
+    fill_grid(wordSearch)
+    grid_print(wordSearch)
