6ab2752fd53418b005d19127ad4834ffeb938f78
andygutterman <andygutterman@outlook.com>
2024-06-22 20:25:36 -0400

    Increased testing capabilities added test_id, increased coverage

--- TestWordSearch.py
+++ TestWordSearch.py
@@ -1,8 +1,4 @@
 from WordSearch import WordSearch
-# todo track word location
-# todo use word location to make sure words dont get
-#  put on top of each other as you will see in runtest3
-# todo system to keep track of found words
 
 class TestWordSearch(WordSearch):
     def __init__(self, size, predefined_words):
@@ -10,20 +6,37 @@ class TestWordSearch(WordSearch):
         self.words = predefined_words
 
 
-def run_test(size, predefined_words):
+def run_test(test_id, size, predefined_words):
     test_word_search = TestWordSearch(size, predefined_words)
-    test_word_search.place_words()
-    test_word_search.fill_grid()
-    test_word_search.show_grid()
-    # test_word_search.show_wordbank()
-    test_word_search.print_word_locations()
+    try:
+        test_word_search.place_words()
+        test_word_search.fill_grid()
+        test_word_search.show_grid()
+        test_word_search.print_word_locations()
+    except Exception as e:
+        print(f"Test {test_id} failed with error: {e}")
 
 if __name__ == '__main__':
     tests = [
-        (10, ["PYTHON", "JAVA", "RUBY", "CPLUSPLUS", "SWIFT", "JAVASCRIPT"]),
-        (5, ["DOGGY", "FOGGY", "LOG", "COG", "DOG", "POG"]),
-        (5, ["DOGGY", "DOGGY", "DOGGY", "DOGGY", "DOGGY"])
+        ("test1", 10, ["PYTHON", "JAVA", "RUBY", "CPLUSPLUS", "SWIFT", "JAVASCRIPT"]),
+        ("test2", 5, ["DOGGY", "FOGGY", "LOG", "COG", "DOG", "POG"]),
+        ("test3", 5, ["DOGGY", "DOGGY", "DOGGY", "DOGGY", "DOGGY"]),
+        ("test4", 7, ["22222", "11111", "11121", "11111", "123456", "515", "515"]),
+        ("test5", 3, ["HELLO", "WORLD", "PYTHON"]),
+        ("test6", 4, ["RED", "GREEN", "BLUE", "YELLOW"]),
+        ("test7", 6, ["APPLE", "ORANGE", "BANANA", "GRAPE", "PEAR", "KIWI"]),
+        ("test8", 2, ["CAR", "BIKE"]),
+        ("test9", 5, ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY"]),
+        ("test10", 4, ["SUN", "MOON", "STAR", "PLANET"]),
+        ("test11", 0, ["ZERO"]),
+        ("test12", -1, ["NEGATIVE"]),
+        ("test13", 3, []),
+        ("test14", "invalid", ["INVALID"]),
+        ("test15", 4, ["VALID", "LIST", "WITH", 12345])
     ]
 
-    for size, words in tests:
-        run_test(size, words)
+    for test_id, size, words in tests:
+        try:
+            run_test(test_id, size, words)
+        except Exception as e:
+            print(f"Test {test_id} failed with error: {e}")
