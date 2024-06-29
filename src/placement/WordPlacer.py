5b44598334cbb58aa69562746b5e75cead6fb8d4
andygutterman <andygutterman@outlook.com>
2024-06-29 16:46:19 -0400

    moved to own method

--- src/placement/WordPlacer.py
+++ src/placement/WordPlacer.py
@@ -1,7 +1,44 @@
 import random
 
-
 class WordPlacer:
+    @staticmethod
+    def place_words(word_search):
+        word_search.words.sort(key=len, reverse=True)
+        max_spaces = word_search.size ** 2
+        big_words_count = sum(1 for word in word_search.words if len(word) == word_search.size)
+        current_grid_state = [row[:] for row in word_search.grid]
+        try:
+            for word in word_search.words:
+                if word in word_search.word_locations:
+                    continue
+                letters = list(word)
+                placed = False
+                attempts = 0
+                max_attempts = 1000
+                while not placed and attempts < max_attempts:
+                    if big_words_count >= 1:
+                        r = random.randint(1, 2)
+                    else:
+                        r = random.randint(1, 4)    # 3 and 4 both diagonal
+                    if r == 1:
+                        direction = "vertical"
+                    elif r == 2:
+                        direction = "horizontal"
+                    elif r == 3 or r == 4:
+                        direction = "diagonal"
+                    placed = WordPlacer.place(word_search, letters, max_spaces, direction)
+                    if placed:
+                        big_words_count -= 1
+                        word_search.word_locations.setdefault(tuple(letters), [])
+                        attempts = 0
+                    else:
+                        attempts += 1
+                if not placed:
+                    print(f"Failed to place word '{word}' after {max_attempts} attempts.")
+
+        except Exception as e:
+            print(f"Error occurred during word placement: {e}")
+            word_search.grid = current_grid_state  # Revert to previous grid state
 
     @staticmethod
     def place(word_search, letters, max_attempts, direction):
